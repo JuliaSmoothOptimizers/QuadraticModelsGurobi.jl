b4ba9f96fd70b45c15aa973d4eb8399a31a84312
Geoffroy Leconte <gleconte50@gmail.com>
2020-09-02 15:42:47 +0200

    test1

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,15 @@ using QPSReader, QuadraticModelsGurobi
 using Test
 
 @testset "QuadraticModelsGurobi.jl" begin
-    qps1 = readqps("QAFIRO.SIF")
+    qps1 = readqps("QAFIRO.SIF") #lower bounds
     stats1 = gurobi(qps1)
-    @test abs(stats1.objective + 1.59078) < 1e-2
+    @test abs(stats1.objective + 1.59078179) < 1e-2
+
+    qps2 = readqps("HS21.SIF") # low/upp bounds
+    stats2 = gurobi(qps2)
+    @test abs(stats2.objective + 9.99599999e1) < 1e-2
+
+    qps3 = readqps("HS52.SIF") # free bounds
+    stats3 = gurobi(qps3)
+    @test abs(stats3.objective - 5.32664756) < 1e-2
 end

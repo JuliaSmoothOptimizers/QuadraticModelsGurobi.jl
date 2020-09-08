64c34d275df8733bcfe4c6255b5f2bfc01c3745d
Geoffroy Leconte <gleconte50@gmail.com>
2020-09-08 12:25:58 +0200

    test sparse_csr

--- src/QuadraticModelsGurobi.jl
+++ src/QuadraticModelsGurobi.jl
@@ -21,46 +21,46 @@ const gurobi_statuses = Dict(:loaded => :unknown,
                              :suboptimal => :exception,
                              :inprogress => :exception,
                              :user_obj_limit => :exception)
-
-function sparse_csr(I, J, V, m=maximum(I), n=maximum(J))
-	csrrowptr = zeros(Int, m+1)
-	# Compute the CSR form's row counts and store them shifted forward by one in csrrowptr
-	coolen = length(I)
-	min(length(J), length(V)) >= coolen || throw(ArgumentError("J and V need length >= length(I) = $coolen"))
-	@inbounds for k in 1:coolen
-		Ik = I[k]
-	if 1 > Ik || m < Ik
-		throw(ArgumentError("row indices I[k] must satisfy 1 <= I[k] <= m"))
-	end
-	csrrowptr[Ik+1] += 1
-	end
-
-	# Compute the CSR form's rowptrs and store them shifted forward by one in csrrowptr
-	countsum = 1
-	csrrowptr[1] = 1
-	@inbounds for i in 2:(m+1)
-		overwritten = csrrowptr[i]
-		csrrowptr[i] = countsum
-		countsum += overwritten
-	end
-
-	# Counting-sort the column and nonzero values from J and V into csrcolval and csrnzval
-	# Tracking write positions in csrrowptr corrects the row pointers
-	csrcolval = zeros(Int, length(I))
-	csrnzval = zeros(length(I))
-	@inbounds for k in 1:coolen
-		Ik, Jk = I[k], J[k]
-		if 1 > Jk || n < Jk
-			throw(ArgumentError("column indices J[k] must satisfy 1 <= J[k] <= n"))
-		end
-		csrk = csrrowptr[Ik+1]
-		csrrowptr[Ik+1] = csrk + 1
-		csrcolval[csrk] = Jk
-		csrnzval[csrk] = V[k]
-	end
-	csrrowptr = csrrowptr[1:end-1]
-	return csrrowptr, csrcolval, csrnzval
-end
+# 
+# function sparse_csr(I, J, V, m=maximum(I), n=maximum(J))
+# 	csrrowptr = zeros(Int, m+1)
+# 	# Compute the CSR form's row counts and store them shifted forward by one in csrrowptr
+# 	coolen = length(I)
+# 	min(length(J), length(V)) >= coolen || throw(ArgumentError("J and V need length >= length(I) = $coolen"))
+# 	@inbounds for k in 1:coolen
+# 		Ik = I[k]
+# 	if 1 > Ik || m < Ik
+# 		throw(ArgumentError("row indices I[k] must satisfy 1 <= I[k] <= m"))
+# 	end
+# 	csrrowptr[Ik+1] += 1
+# 	end
+#
+# 	# Compute the CSR form's rowptrs and store them shifted forward by one in csrrowptr
+# 	countsum = 1
+# 	csrrowptr[1] = 1
+# 	@inbounds for i in 2:(m+1)
+# 		overwritten = csrrowptr[i]
+# 		csrrowptr[i] = countsum
+# 		countsum += overwritten
+# 	end
+#
+# 	# Counting-sort the column and nonzero values from J and V into csrcolval and csrnzval
+# 	# Tracking write positions in csrrowptr corrects the row pointers
+# 	csrcolval = zeros(Int, length(I))
+# 	csrnzval = zeros(length(I))
+# 	@inbounds for k in 1:coolen
+# 		Ik, Jk = I[k], J[k]
+# 		if 1 > Jk || n < Jk
+# 			throw(ArgumentError("column indices J[k] must satisfy 1 <= J[k] <= n"))
+# 		end
+# 		csrk = csrrowptr[Ik+1]
+# 		csrrowptr[Ik+1] = csrk + 1
+# 		csrcolval[csrk] = Jk
+# 		csrnzval[csrk] = V[k]
+# 	end
+# 	csrrowptr = csrrowptr[1:end-1]
+# 	return csrrowptr, csrcolval, csrnzval
+# end
 
 function gurobi(QM; method=2, kwargs...)
     env = Gurobi.Env()
@@ -97,16 +97,16 @@ function gurobi(QM; method=2, kwargs...)
 	  add_qpterms!(model, QM.data.Hrows, QM.data.Hcols, Hvals)
 	end
 
-	Acsrrowptr, Acsrcolval, Acsrnzval = sparse_csr(QM.data.Arows,QM.data.Acols,
-												   QM.data.Avals, QM.meta.ncon,
-												   QM.meta.nvar)
+	# Acsrrowptr, Acsrcolval, Acsrnzval = sparse_csr(QM.data.Arows,QM.data.Acols,
+	# 											   QM.data.Avals, QM.meta.ncon,
+	# 											   QM.meta.nvar)
 
-	# add_rangeconstrs!(model, sparse(QM.data.Arows, QM.data.Acols,
-	# 								  QM.data.Avals, QM.meta.ncon,
-	# 								  QM.meta.nvar),
-	# 					QM.meta.lcon, QM.meta.ucon)
-	add_rangeconstrs!(model, Acsrrowptr, Acsrcolval, Acsrnzval, QM.meta.lcon,
-				 	  QM.meta.ucon)
+	add_rangeconstrs!(model, sparse(QM.data.Arows, QM.data.Acols,
+									  QM.data.Avals, QM.meta.ncon,
+									  QM.meta.nvar),
+						QM.meta.lcon, QM.meta.ucon)
+	# add_rangeconstrs!(model, Acsrrowptr, Acsrcolval, Acsrnzval, QM.meta.lcon,
+	# 			 	  QM.meta.ucon)
 
 	update_model!(model)
 

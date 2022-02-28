51ef4b887b350e57d84d23f9c8c77c8a6823188e
MonssafToukal <32516773+MonssafToukal@users.noreply.github.com>
2022-02-28 14:41:51 -0500

    Up unit test setup (#16)
    * up unit test setup
    
    * fix typo in jenkinsfile token

--- test/send_gist_url.jl
+++ test/send_gist_url.jl
@@ -12,19 +12,16 @@ TEST_RESULTS_FILE = "$(ORG)_$(REPO)_$(PR).txt"
 myauth = GitHub.authenticate(ENV["GITHUB_AUTH"])
 
 function create_gist(authentication)
-    
-    file_content = ""
-    file = open(TEST_RESULTS_FILE, "r")
-    for line in readlines(file)
-        file_content *= line*'\n'
-    end
-    close(file)
-    
+
+    file_content = read(TEST_RESULTS_FILE, String)
+
     file_dict = Dict(TEST_RESULTS_FILE => Dict("content" => file_content))
-    gist = Dict{String,Any}("description" => "Test results",
-                             "public" => true,
-                             "files" => file_dict)
-    
+    gist = Dict{String,Any}(
+        "description" => "Test results",
+        "public" => true,
+        "files" => file_dict,
+    )
+
     posted_gist = GitHub.create_gist(params = gist, auth = authentication)
 
     return posted_gist
@@ -40,14 +37,33 @@ end
 function get_repo(api::GitHub.GitHubWebAPI, org::String, repo_name::String; kwargs...)
     my_params = Dict(:visibility => "all")
     # return GitHub.repo(api, repo; params = my_params, kwargs...)
-    return Repo(GitHub.gh_get_json(api, "/repos/$(org)/$(repo_name)"; params = my_params, kwargs...))
+    return Repo(
+        GitHub.gh_get_json(
+            api,
+            "/repos/$(org)/$(repo_name)";
+            params = my_params,
+            kwargs...,
+        ),
+    )
 end
 
-function get_pull_request(api::GitHub.GitHubWebAPI, org::String, repo::Repo, pullrequest_id; kwargs...)
-    my_params = Dict(:sort => "popularity",
-                    :direction => "desc")
-    pull_request = PullRequest(GitHub.gh_get_json(api, "/repos/$(org)/$(repo.name)/pulls/$(pullrequest_id)"; params=my_params, kwargs...))
-   
+function get_pull_request(
+    api::GitHub.GitHubWebAPI,
+    org::String,
+    repo::Repo,
+    pullrequest_id;
+    kwargs...,
+)
+    my_params = Dict(:sort => "popularity", :direction => "desc")
+    pull_request = PullRequest(
+        GitHub.gh_get_json(
+            api,
+            "/repos/$(org)/$(repo.name)/pulls/$(pullrequest_id)";
+            params = my_params,
+            kwargs...,
+        ),
+    )
+
     return pull_request
 end
 
